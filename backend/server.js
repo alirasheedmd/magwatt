5468862041a3b80d77fd7df72549ce35b8043916
Muhammad Ali Rasheed <alirasheedmd@gmail.com>
2021-05-20 18:30:15 +0500

    final commit

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,6 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("API is running")
-})
-
 app.use("/api/products", productRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/orders", orderRoutes)
@@ -36,6 +32,19 @@ const __dirname = path.resolve() //as we are using es6 js we have to mimic.
 //Node.js blocks anyone reading files from its directory so what we will do is that we will ask node.js to make upload file available by making it static file.
 //__dirname is the main directory from where we want to upload.
 app.use("/uploads", express.static(path.join(__dirname, "/uploads"))) //path.join(__dirname, "/uploads") is just taking us to the uploads folder
+
+//when we deploy our webapp on the website we want index.html file to run from the build folder.
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")))
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  )
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running")
+  })
+}
 
 app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID)
